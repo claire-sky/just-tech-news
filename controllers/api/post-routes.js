dc97c64ef0763316d3ea3f54ba0c8ecc4c587931
Claire Sky <eclconnolly@gmail.com>
2022-01-21 13:51:37 -0800

    single post handlebars and route

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -90,6 +90,8 @@ router.post('/', (req, res) => {
 });
 
 router.put('/upvote', (req, res) => {
+  // make sure the session exists first
+  if (req.session) {
   // custom static method created in models/Post.js
   Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
     .then(updatedVoteData => res.json(updatedVoteData))
@@ -97,6 +99,7 @@ router.put('/upvote', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+  }
 });
 
 router.put('/:id', (req, res) => {
